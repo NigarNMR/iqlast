c727fb56cc1702a57a0945a408653129bbdb5da6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-04 14:33:20 +0000

    Updating to latest FlutterFlow output.

--- android/app/src/main/kotlin/com/example/eduline_ui_design/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eduline_ui_design
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.iqink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
